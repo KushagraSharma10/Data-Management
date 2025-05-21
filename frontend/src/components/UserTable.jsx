6b7acef57e9851470914becaa4e9a33c3002f560
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-05-22 01:56:35 +0530

    single check at a time functionality added inside userTable

--- frontend/src/components/UserTable.jsx
+++ frontend/src/components/UserTable.jsx
@@ -268,22 +268,11 @@ export default function UserTable() {
   };
 
   const handleClick = (event, id) => {
-    const selectedIndex = selected.indexOf(id);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, id);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
+    if (selected.length === 1 && selected[0] === id) {
+      setSelected([]);
+    } else {
+      setSelected([id]);
     }
-    setSelected(newSelected);
   };
 
   const handleChangePage = (event, newPage) => {
